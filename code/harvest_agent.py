c2f8b5d777327d37a02d114f38f2220ac1db71d1
AmyJose <helloamyjose@gmail.com>
2026-06-05 11:54:15 +0100

    model initialisation

--- code/harvest_agent.py
+++ code/harvest_agent.py
@@ -1,10 +1,15 @@
-from mesa.discrete_space import CellAgent
+from mesa import Agent
 
-class HarvestAgent(CellAgent):
+class HarvestAgent(Agent):
     """Agent in the model environment"""
 
-    def __init__(self, id):
-        super.__init__()
+    def __init__(self, model, id):
+        super().__init__(model)
 
         self.id = id
-        
+
+        self.health = 10
+        self.berries = 0
+
+    def step(self):
+        self.health -= 1
--- code/harvest_model.py
+++ code/harvest_model.py
@@ -1,18 +1,42 @@
 import mesa
 from mesa.discrete_space import OrthogonalVonNeumannGrid
+from harvest_agent import HarvestAgent
 
 class HarvestModel(mesa.Model):
     """Harvest environemnt for resource sharing"""
-    def __init__(self, num_agents, seed, width, height):
+    def __init__(self, seed, num_agents=4, num_berries=15, width=10, height=10):
         super().__init__(seed=seed)
-
-        self.num_agents = num_agents
 
         self.grid = OrthogonalVonNeumannGrid(
             [width, height],
             torus=False,
             random=self.random
         )
+
+        self.num_agents = num_agents
+        self.num_berries = num_berries
+        self.berries = set()
+
+        self.spawn_berries()
+
+        self.agents = []
+        for i in range(self.num_agents):
+            agent = HarvestAgent(self, i)
+
+            cell = self.random.choice(list(self.grid.all_cells.cells))
+            self.grid.place_agent(agent, cell)
+
+            self.agents.append(agent)
+        
     
     def step(self):
         self.agents.shuffle_do("step")
+
+    def spawn_berries(self):
+        self.berries.clear()
+
+        for _ in range(self.num_berries):
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+
+            self.berries.add((x, y))
